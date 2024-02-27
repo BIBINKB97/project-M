a198f11d9e95465951aa3b4742e13178578a71e3
BIBINKB97 <bibinkochumalayil007@gmail.com>
2024-02-27 17:52:52 +0530

    made some changes

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,28 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:project_m/utils/utils.dart';
+import 'package:project_m/view/widgets/text_style.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen
+    extends StatefulWidget {
+  const HomeScreen(
+      {Key?
+          key})
+      : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen>
+      createState() =>
+          _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  List<bool> motorStates = List<bool>.generate(2, (index) => false);
-  List<bool> valveStates = List<bool>.generate(50, (index) => false);
+class _HomeScreenState
+    extends State<
+        HomeScreen> {
+  List<bool>
+      motorStates =
+      List<bool>.generate(2,
+          (index) => false);
+  List<bool>
+      valveStates =
+      List<bool>.generate(50,
+          (index) => false);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+      BuildContext
+          context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Control Panel"),
+      appBar:
+          AppBar(
+        title: CustomText(
+          text: "Control Panel",
+          fs: 25,
+          fw: FontWeight.w700,
+          color: kblack,
+        ),
         centerTitle: true,
       ),
-      body: Container(
+      body:
+          Container(
         margin: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            Text("Motor"),
+            CustomText(
+              text: "Motor's",
+              fs: 23,
+              fw: FontWeight.w500,
+              color: kblack,
+            ),
             Container(
               height: 200,
               width: double.infinity,
@@ -41,14 +71,25 @@ class _HomeScreenState extends State<HomeScreen> {
                         color: motorStates[index] ? Colors.green : Colors.red,
                         borderRadius: BorderRadius.circular(10),
                       ),
-                      child: Center(child: Text("Motor ${index + 1}")),
+                      child: Center(
+                          child: CustomText(
+                        text: "Motor ${index + 1}\n\t  ${motorStates[index] ? "OFF" : "ON"}",
+                        fs: 20,
+                        fw: FontWeight.w700,
+                        color: kwhite,
+                      )),
                     ),
                   );
                 },
                 itemCount: 2,
               ),
             ),
-            Text("Valves"),
+            CustomText(
+              text: "Valve's",
+              fs: 23,
+              fw: FontWeight.w500,
+              color: kblack,
+            ),
             Expanded(
               child: GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
@@ -65,7 +106,13 @@ class _HomeScreenState extends State<HomeScreen> {
                         color: valveStates[index] ? Colors.green : Colors.red,
                         borderRadius: BorderRadius.circular(10),
                       ),
-                       child: Center(child: Text("Valve ${index + 1}")),
+                      child: Center(
+                          child: CustomText(
+                        text: "Valve ${index + 1}\n\t${valveStates[index] ? "Close" : "Open"}",
+                        fs: 15,
+                        fw: FontWeight.w700,
+                        color: kwhite,
+                      )),
                     ),
                   );
                 },
